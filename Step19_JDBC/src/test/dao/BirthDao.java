b40793f20ed93b681c9070eb7c024b831e522c83
keunyeong <91kyeong@gmail.com>
2021-05-17 16:52:43 +0900

    2021051717:00study

--- Step19_JDBC/src/test/dao/BirthDao.java
+++ Step19_JDBC/src/test/dao/BirthDao.java
@@ -2,6 +2,9 @@ package test.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import test.dto.BirthDto;
 import test.util.DBConnect;
@@ -14,6 +17,127 @@ public class BirthDao {
 			dao = new BirthDao();
 		}
 		return dao;
+	}
+	public BirthDto getData(int num) {
+		//회원 한명의 정보를 담을 MemberDto 
+		BirthDto dto = null;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//DBConnect 객체를 이용해서 Connection 객체의 참조값을 얻어온다.
+			conn = new DBConnect().getConn();
+			//실행할 sql 문
+			String sql = "SELECT name,birthday" + " FROM birth" + " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			//query 문 수행하고 결과 얻어오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 select 된 회원정보  읽어오기
+			while (rs.next()) {
+				//MemberDto 객체 생성해서 
+				dto = new BirthDto();
+				//회원 한명의 정보를 담는다. 
+				dto.setNum(num);
+				dto.setName(rs.getString("name"));
+				dto.setBirthday(rs.getString("birthday"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				//객체를 사용했던 순서 역순으로 닫아준다.
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+
+		return dto;
+	}
+	public List<BirthDto> getList() {
+		//회원 목록을 담을 객체 생성
+		List<BirthDto> list = new ArrayList<>();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//DBConnect 객체를 이용해서 Connection 객체의 참조값을 얻어온다.
+			conn = new DBConnect().getConn();
+			//실행할 sql 문
+			String sql = "SELECT num,name,birthday" + " FROM birth" + " ORDER BY num ASC";
+			pstmt = conn.prepareStatement(sql);
+			//query 문 수행하고 결과 얻어오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 select 된 회원정보  읽어오기
+			while (rs.next()) {
+				//회원정보를 list 에 담아 보세요.
+				int num = rs.getInt("num");
+				String name = rs.getString("name");
+				String birthday = rs.getString("birthday");
+				//MemberDto 객체를 생성해서 회원 한명의 정보를 담는다.
+				BirthDto dto = new BirthDto();
+				dto.setNum(num);
+				dto.setName(name);
+				dto.setBirthday(birthday);
+				//MemberDto 객체를 List 에 누적 시킨다.
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				//객체를 사용했던 순서 역순으로 닫아준다.
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+
+		return list;
+	}
+	public boolean update(BirthDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DBConnect().getConn();
+			//실행할 sql(Insert or Update or Delete) 문 작성
+			String sql = "update memo"
+					+ " set name = ?,birthday = ?"
+					+ " where num = ?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 바인딩할 내용이 있으면 여기서 한다.
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getBirthday());
+			pstmt.setInt(3, dto.getNum());
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	public boolean insert(BirthDto dto) {
 		Connection conn = null;
@@ -54,10 +178,11 @@ public class BirthDao {
 		try {
 			conn = new DBConnect().getConn();
 			//실행할 sql(Insert or Update or Delete) 문 작성
-			String sql = "delete from   ";
+			String sql = "delete from birth"
+					+ " where num = ?";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 바인딩할 내용이 있으면 여기서 한다.
-
+			pstmt.setInt(1, num);
 			flag = pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
---
+++ Step19_JDBC/src/test/frame/birthFrame.java
@@ -0,0 +1,146 @@
+package test.frame;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import test.dao.BirthDao;
+import test.dto.BirthDto;
+
+public class birthFrame extends JFrame 
+				implements ActionListener,PropertyChangeListener{
+	JTextField inputName, inputBirthday;
+	DefaultTableModel model;
+	JTable table;
+	
+	public birthFrame() {
+		setLayout(new BorderLayout());
+		
+		JLabel label1=new JLabel("이름");
+		inputName=new JTextField(10);
+		
+		JLabel label2=new JLabel("주소");
+		inputBirthday=new JTextField(10);
+		
+		JButton saveBtn=new JButton("저장");
+		saveBtn.setActionCommand("save");
+		saveBtn.addActionListener(this);
+		
+		JButton deleteBtn=new JButton("삭제");
+		deleteBtn.setActionCommand("delete");
+		deleteBtn.addActionListener(this);
+		
+		JPanel panel=new JPanel();
+		panel.add(label1);
+		panel.add(inputName);
+		panel.add(label2);
+		panel.add(inputBirthday);
+		panel.add(saveBtn);
+		panel.add(deleteBtn);
+		
+		add(panel, BorderLayout.NORTH);
+		
+		table=new JTable();
+		String[] colNames= {"번호", "이름", "생일"};
+		model=new DefaultTableModel(colNames, 0) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				if(column==0) {
+					return false;
+				}
+				return true;
+			}
+		};
+		table.setModel(model);
+		JScrollPane scroll=new JScrollPane(table);
+		add(scroll, BorderLayout.CENTER);
+		displayMember();
+		table.addPropertyChangeListener(this);
+	}
+	public void displayMember() {
+		model.setRowCount(0);
+		BirthDao dao=BirthDao.getInstance();
+		List<BirthDto> list=dao.getList();
+		for(BirthDto tmp:list) {
+			Object[] row= {tmp.getNum(), tmp.getName(), tmp.getBirthday()};
+			model.addRow(row);
+		}
+	}
+	
+	public static void main(String[] args) {
+		birthFrame f=new birthFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setBounds(100, 100, 800, 500);
+		f.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String command=e.getActionCommand();
+		if(command.equals("save")) {
+			String name=inputName.getText();
+			String birthday=inputBirthday.getText();
+			BirthDto dto=new BirthDto();
+			dto.setName(name);
+			dto.setBirthday(birthday);
+			BirthDao dao=BirthDao.getInstance();
+			boolean isSuccess=dao.insert(dto);
+			if(isSuccess) {
+				JOptionPane.showMessageDialog(this, name+" 님의 정보 추가 했습니다.");
+			}else {
+				JOptionPane.showMessageDialog(this, "추가 실패!");
+			}
+			displayMember();
+		}else if(command.equals("delete")) {
+			int selectedIndex=table.getSelectedRow();
+			if(selectedIndex==-1) {
+				return;
+			}
+			
+			int selection=JOptionPane.showConfirmDialog(this, "선택된 row 를 삭제하겠습니까?");
+			if(selection != JOptionPane.YES_OPTION) {
+				return;
+			}
+			int num=(int)model.getValueAt(selectedIndex, 0);
+			BirthDao dao=BirthDao.getInstance();
+			dao.delete(num);
+			displayMember();
+		}
+	}
+	boolean isEditing=false;
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		System.out.println("property change!");
+		System.out.println(evt.getPropertyName());
+		if(evt.getPropertyName().equals("tableCellEditor")) {
+			if(isEditing) {//수정중일때 
+				//변화된 값을 읽어와서 DB 에 반영한다. 
+				//수정된 칼럼에 있는 row  전체의 값을 읽어온다. 
+				int selectedIndex=table.getSelectedRow();
+				int num=(int)model.getValueAt(selectedIndex, 0);
+				String name=(String)model.getValueAt(selectedIndex, 1);
+				String birthday=(String)model.getValueAt(selectedIndex, 2);
+				//수정할 회원의 정보를 MemberDto 객체에 담고 
+				BirthDto dto=new BirthDto(num, name, birthday);
+				//DB에 저장하기 
+				BirthDao.getInstance().update(dto);
+				isEditing=false;
+			}
+			isEditing=true;
+		}
+	}
+}
